b48f6f4f09abf2ecf7990d65b30235bb77265309
robbey@deepsight.ai <robbey@deepsight.ai>
2021-01-25 09:30:07 +0800

    #update: trival

--- test_qml.cpp
+++ test_qml.cpp
@@ -4,6 +4,6 @@ using namespace rea;
 
 static regPip<QJsonObject> test_qml([](stream<QJsonObject>* aInput){
     if (aInput->data().value("qml").toBool())
-        pipeline::instance()->engine->load(QUrl(QStringLiteral("gui/test.qml")));
+        pipeline::instance()->engine->load(QUrl(QStringLiteral("test.qml")));
     aInput->out();
 },  QJsonObject(), "unitTest");
